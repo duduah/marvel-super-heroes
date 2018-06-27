44dd7ad996062e47d3fcf105bce891e7ada1a1ea
Diego Gay Saez <diego.gay@gmail.com>
2018-06-27 22:13:04 +0200

    Added ViewModel and ViewModel.Factory for Marvel Heroes list. WIP = Integrate view model in activity

---
+++ app/src/main/java/com/costular/marvelheroes/presentation/heroeslist/HeroesListViewModel.kt
@@ -0,0 +1,31 @@
+package com.costular.marvelheroes.presentation.heroeslist
+
+import android.arch.lifecycle.MutableLiveData
+import com.costular.marvelheroes.data.repository.MarvelHeroesRepository
+import com.costular.marvelheroes.domain.model.MarvelHeroEntity
+import com.costular.marvelheroes.util.mvvm.BaseViewModel
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.rxkotlin.addTo
+import io.reactivex.rxkotlin.subscribeBy
+import io.reactivex.schedulers.Schedulers
+import javax.inject.Inject
+
+class HeroesListViewModel @Inject constructor(val marvelHeroesRepository: MarvelHeroesRepository)
+    : BaseViewModel() {
+
+    val heroesListState : MutableLiveData<List<MarvelHeroEntity>> = MutableLiveData()
+
+    fun loadHeroesList() {
+        marvelHeroesRepository
+                .getMarvelHeroesList()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeBy(
+                        onNext = {
+                            heroesListState.value = it
+                        },
+                        onError = {}
+                )
+                .addTo(compositeDisposable)
+    }
+}
